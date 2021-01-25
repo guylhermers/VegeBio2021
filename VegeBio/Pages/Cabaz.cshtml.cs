0e8c5eb2c2a6bce69e999c688da66ad8d43830d7
paxadax <pedroazevedoduarte@gmail.com>
2021-01-25 21:08:40 +0000

    Cabaz_order
    Cabaz_order

--- VegeBio/Pages/Cabaz.cshtml.cs
+++ VegeBio/Pages/Cabaz.cshtml.cs
@@ -1,16 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VegeBio.Data;
+using VegeBio.Models;
+
 
 namespace VegeBio.Pages
 {
     public class CabazModel : PageModel
     {
-        public void OnGet()
-        {
+      
+            private readonly VegeBioContext _context;
+            public CabazModel(VegeBioContext context)
+            {
+                _context = context;
+            }
+            [Required(ErrorMessage = "Email Obrigatório")]
+            [BindProperty]
+            public string CustomerEmail { get; set; }
+            [Required(ErrorMessage = "Morada Obrigatória")]
+            [BindProperty]
+            public string ShippingAddress { get; set; }
+            [Required(ErrorMessage = "Quantidade Obrigatória")]
+            [BindProperty]
+            public int OrderQt { get; set; } = 1;
+            public Produto Produto { get; set; }
         }
-    }
 }
