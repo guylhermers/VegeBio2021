801dc172f47002aa6fb609784b9160ca936372d1
guylhermers <73615448+guylhermers@users.noreply.github.com>
2021-01-25 19:23:25 +0000

    Classes

--- VegeBio/Models/Cliente.cs
+++ VegeBio/Models/Cliente.cs
@@ -7,5 +7,16 @@ namespace VegeBio.Models
 {
     public class Cliente
     {
+        private int id;
+        private string nome;
+        private string morada;
+        private int nif;
+
+
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Morada { get => morada; set => morada = value; }
+        public int Nif { get => nif; set => nif = value; }
     }
 }
