ef0b452979040afe783b9d54ef67f8a24b2a3701
guylhermers <73615448+guylhermers@users.noreply.github.com>
2021-01-25 18:22:40 +0000

    Projeto Inserido

---
+++ VegeBio/Models/ListaProdutos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VegeBio.Models
+{
+    public class ListaProdutos : Produto
+    {
+        private List<Produto> listaProduto;
+        private string epoca;
+        private int id;
+
+        public ListaProdutos(string epoca, int id)
+        {
+            this.epoca = epoca;
+            this.id = id;
+        }
+
+        public void adicionarProduto(Produto produto)
+        {
+            ListaProduto.Add(produto);
+        }
+        public void
+
+        public List<Produto> ListaProduto { get => listaProduto; set => listaProduto = value; }
+        public string Epoca { get => epoca; set => epoca = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
---
+++ VegeBio/Models/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VegeBio.Models
+{
+    public class Produto : ListaProdutos
+    {
+        private int id;
+
+        private string epoca;
+
+        private float preco;
+
+        private string tipo;
+
+
+        public int ID { get => id; set => id = value; }
+        public string Epoca { get => epoca; set => epoca = value; }
+        public float Preco { get => preco; set => preco = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+
+        public Produto(int id, string epoca, float preco, string tipo)
+        {
+            this.id = id;
+            this.epoca = epoca;
+            this.preco = preco;
+            this.tipo = tipo;
+        }
+
+        public verificarEpoca()
+        {
+
+        }
+    }
+}
